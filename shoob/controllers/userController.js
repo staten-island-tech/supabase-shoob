af4184400cf03748a20f2af0060e11710b7ee814
irene <iivyleaves@gmail.com>
2025-04-10 12:30:29 -0400

    ??????????? issac will not fail his macro

--- shoob/controllers/userController.js
+++ shoob/controllers/userController.js
@@ -1,32 +1,47 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from 'firebase/auth'
 import { auth } from './firebaseConfig'
-import { signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
 
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
+async function registerUser(email, password) {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password) //automatically comes with uid, email, and things
+    //above is literally all the code we need
     const user = userCredential.user
     console.log('registered:', user)
-  })
-  .catch((error) => {
-    console.error('rrror:', error.message)
-  })
+  } catch (error) {
+    console.error('register error:', error.message)
+  }
+}
 
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
+async function loginUser(email, password) {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password)
     const user = userCredential.user
     console.log('logged in:', user.email)
-  })
-  .catch((error) => {
-    console.error('login rrror:', error.message)
-  })
-
-//add pinia things below or firebase
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-  } else {
+  } catch (error) {
+    console.error('login error:', error.message)
   }
-})
+}
 
-signOut(auth).then(() => {
-  console.log('signed out')
-})
+function monitorAuthState() {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      console.log('user is signed in:', user.email)
+    } else {
+      console.log('user is signed out')
+    }
+  })
+}
+
+async function logoutUser() {
+  try {
+    await signOut(auth)
+    console.log('signed out')
+  } catch (error) {
+    console.error('sign out error:', error.message)
+  }
+}
