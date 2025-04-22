00c3bba6e87402ade19d5b5ec61b874a4ffb93a8
irene <iivyleaves@gmail.com>
2025-04-22 12:12:16 -0400

    delete

--- shoob/src/router/index.js
+++ shoob/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/GameLobby.vue'
 import LoginPage from '../views/LoginPage.vue'
-import GameRoom from '@/views/GameRoom.vue'
+import GameRoom from '../views/GameRoom.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/login',
       name: 'login',
       component: LoginPage,
     },
