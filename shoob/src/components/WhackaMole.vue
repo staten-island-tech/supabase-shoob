c3c4eadcb1957dcf9cf7610a1f36b96ab57f0d42
inconveniencestore <iivyleaves@gmail.com>
2025-06-07 22:53:24 -0400

    plans were made

---
+++ shoob/src/components/WhackaMole.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+//first in list is the vip
+//vip generates a string in the beginning of the game that tells them where the moles are and which ones are bombs?
+//whatever
+//then firebase starts sending the moles to people
+//give the moles ids so that way once its clicked, it gets poofed instantly
+</script>
+
+<style scoped></style>
